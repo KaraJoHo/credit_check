0a4bee946f2e3590f1c5aa48e11d29d287dd2b5c
KaraJH <karas.jones@yahoo.com>
2023-09-13 16:18:28 -0600

    fix: delete flash message attempt

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -2,10 +2,7 @@ class CreditCardsController < ApplicationController
   def show 
     @credit_card = CreditCard.find(params[:id])
 
-    # if @credit_card 
-    #   @credit_card
-    # else 
     #   flash[:error] = @credit_card.errors.full_messages.to_sentence
-    # end
+  
   end
 end
