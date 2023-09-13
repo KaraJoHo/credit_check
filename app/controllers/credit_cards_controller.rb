0cdeeace7a71fb1f3431532ca4824de5eca8afa9
KaraJH <karas.jones@yahoo.com>
2023-09-13 16:17:40 -0600

    fix: comment out flash message

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,15 +1,11 @@
 class CreditCardsController < ApplicationController 
   def show 
     @credit_card = CreditCard.find(params[:id])
-    require 'pry'; binding.pry
 
-    if @credit_card 
-      @credit_card
-    else 
-      flash[:error] = @credit_card.errors.full_messages.to_sentence
-    end
-
-
+    # if @credit_card 
+    #   @credit_card
+    # else 
+    #   flash[:error] = @credit_card.errors.full_messages.to_sentence
+    # end
   end
-
 end
