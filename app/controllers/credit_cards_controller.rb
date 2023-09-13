3e5ae2d6416af81a332c49f3e4ab02930eabbddb
KaraJH <karas.jones@yahoo.com>
2023-09-13 16:14:25 -0600

    attempt to add flash messages to sad path test

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,6 +1,15 @@
 class CreditCardsController < ApplicationController 
   def show 
     @credit_card = CreditCard.find(params[:id])
+    require 'pry'; binding.pry
+
+    if @credit_card 
+      @credit_card
+    else 
+      flash[:error] = @credit_card.errors.full_messages.to_sentence
+    end
+
+
   end
 
 end
