6e5d3bcd7e14eb9ee22ac95f762a5a2be23e34d0
KaraJH <karas.jones@yahoo.com>
2023-09-26 10:32:07 -0600

    fix: allow for error when user doesnt exist

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,8 +1,5 @@
 class CreditCardsController < ApplicationController 
   def show 
     @credit_card = CreditCard.find(params[:id])
-
-    #   flash[:error] = @credit_card.errors.full_messages.to_sentence
-  
   end
 end
